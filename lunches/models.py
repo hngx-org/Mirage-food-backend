df8f5f98026cd01531533865d0983ceb57c8ab7c
Ayomide <ayomidet905@gmail.com>
2023-09-19 22:36:39 +0100

    Created model

--- lunches/models.py
+++ lunches/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import User
+
+class Lunches(models.Model):
+    sender = models.ForeignKey('Users', on_delete=models.CASCADE)
+    receiver = models.ForeignKey('Users', on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    redeemed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    note = models.CharField(max_length=128)
+
+
 
 # Create your models here.
