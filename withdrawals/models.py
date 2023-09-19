d4a8392cf827aed44f25abcbc8d673994732c17b
21stPhenom <phenom.h.acks21@gmail.com>
2023-09-20 00:04:16 +0100

    created model for withdrawals

--- withdrawals/models.py
+++ withdrawals/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Withdrawal(models.Model):
+    status_choices = [
+        ('pending', "processing"),
+        ('completed', 'done')
+    ]
+    user_id = models.CharField(verbose_name='user ID', max_length=200)
+    status = models.CharField(verbose_name='withdrawal status', choices=status_choices, max_length=20)
+    amount = models.BigIntegerField(verbose_name='withdrawal amount')
+    created_at = models.DateTimeField(verbose_name='withdrawal timestamp', auto_now_add=True)
+
+    def __str__(self):
+        return f"Withdrawal #{self.id}"
