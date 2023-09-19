e088093bc166cdc00d5a81cc028bcea76716659f
Ayomide <ayomidet905@gmail.com>
2023-09-20 00:38:37 +0100

    Created organization model

--- lunches/models.py
+++ lunches/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class Organization(models.Model):
+    name = models.CharField(max_length=27)
+    lunch_price = models.DecimalField(decimal_places=3)
+    currency = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
