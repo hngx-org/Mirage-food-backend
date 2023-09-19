4d101682600a90e12af30c651ae3b22fe768ef8f
Mojolajah Adekunle <adekunlemojolajah@gmail.com>
2023-09-19 22:35:22 +0100

    Correcting url in url.py and modigying views.py

--- users/views.py
+++ users/views.py
@@ -10,14 +10,13 @@ from rest_framework import status
 
 @csrf_exempt
 @api_view(['GET'])
-def get_user(request, pk, *args, **kwargs):
+@permissionClasses([IsAuthenticated])
+def get_user(request, *args, **kwargs):
     """
     Get a particular user from user_id or email
     """
-    if pk.isdigit():
-        user = get_object_or_404(User, pk=pk)
-    else:
-        user = get_object_or_404(User, email=pk)
+    id = request.GET.get("id")
+    user = get_object_or_404(User, pk=id)
     serializer = UserSerializer(user)
     return Response({
         "message": "User found",
