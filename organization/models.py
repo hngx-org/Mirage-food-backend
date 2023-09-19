d20c6e675ba8198549ce3248f0ed41b1d349bd23
Ayomide <ayomidet905@gmail.com>
2023-09-20 00:57:59 +0100

    Changed decimal places and blank=False

--- organization/models.py
+++ organization/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 class Organization(models.Model):
-    name = models.CharField(max_length=27)
-    lunch_price = models.DecimalField(decimal_places=3)
-    currency = models.CharField(max_length=10)
+    name = models.CharField(max_length=27, blank=False)
+    lunch_price = models.DecimalField(decimal_places=2)
+    currency = models.CharField(max_length=10, blank=False)
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Organizations'
 
 
 # Create your models here.
