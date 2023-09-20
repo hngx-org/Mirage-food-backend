77d397a72e2d1c858cbee83734c00378f9cf2f3a
Maryann Mwikali <mwikali119@gmail.com>
2023-09-20 21:05:23 +0300

    Change tiny user details

--- users/serializers.py
+++ users/serializers.py
@@ -1,13 +1,13 @@
-from .models import CustomUser
+from .models import User
 from rest_framework import serializers
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ('id', 'email', 'password', 'first_name', 'last_name', )
         extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
         
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
         return user
