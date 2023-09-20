2676975fa3f8d8995dd3c7867bdc3f68fe3ea1a4
Maryann Mwikali <mwikali119@gmail.com>
2023-09-20 23:13:24 +0300

    Add org lunch wallet

--- organization/models.py
+++ organization/models.py
@@ -16,11 +16,14 @@ class Organization(models.Model):
     class Meta:
         verbose_name_plural = 'Organizations'
 
-
-        
 class OrganizationLunchWallet(models.Model):
   """Model for Organization Lunch Wallet"""
   balance = models.DecimalField(max_digits=10,decimal_places=2,validators=[MinValueValidator(0)])
   org_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+        return f"{self.balance}"
+
+
