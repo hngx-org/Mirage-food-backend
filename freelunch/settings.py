35ad6b293df637e2480541626bb02d34987944e5
unknown <abdullahishuaibumaje.com>
2023-09-20 13:27:09 -0700

    making adjustments

--- freelunch/settings.py
+++ freelunch/settings.py
@@ -24,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'rest_framework',
+    'lunches'
 ]
 
 MIDDLEWARE = [
