488ef7d4ec011993de38bd307b4ccf7c920ed2b1
Nabil Affo <nabil.noble01@gmail.com>
2023-09-20 19:56:00 +0000

    correct import of models in organization/serializers.py

--- organization/serializers.py
+++ organization/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from models import OrganizationLunchWallet
+from .models import OrganizationLunchWallet
 
 
 class OrganizationLunchWalletSerializer(serializers.ModelSerializer):
