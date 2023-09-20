d2eb2d49de272a9d73b2268b5ec7527b2db95a26
Pundits-iTech <mohammedaalli088@gmail.com>
2023-09-20 15:28:21 +0000

    user update

--- users/serializers.py
+++ users/serializers.py
@@ -17,3 +17,17 @@ class UserListSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'org_id', 'first_name', 'last_name', 'profile_pic', 'email', 'phone', 'created_at', 'updated_at', 'lunch_credit_balance']
+
+
+class UserRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'phone', ' profile_pic ',
+                  'bank_name', 'bank_number', 'bank_code', 'bank_region', 'currency', 'currency_code',
+                    'created_at', 'updated_at', 'lunch_credit_balance'
+                  ]
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
