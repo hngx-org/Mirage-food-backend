6163b7fcb3c5153d4c2a40945ab3bbac8a3b5041
Pundits-iTech <mohammedaalli088@gmail.com>
2023-09-20 13:36:24 +0000

    user update

--- users/views.py
+++ users/views.py
@@ -23,12 +23,31 @@ class UserListViewSet(APIView):
             "data": serializer.data
         }, status=status.HTTP_200_OK)
     
-def put(selt,request, *args, **kwargs):
+    def put(selt,request, *args, **kwargs):
         
         '''
         updating user details
         '''
         queryset = User.objects.all()
+        serializer_class = UserListSerializer()
+        user_id = kwargs.get('pk')
+
+    # checking if the user exist
+
+        try:
+            User = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return Response({"status": False, "message": f"The user with user_id {user_id} does not exist."},
+                            status=status.HTTP_404_NOT_FOUND)
+        serializer = serializer_class(User, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": True, "message": f"Person with user_id {user_id} updated successfully."},
+                            status=status.HTTP_200_OK)
+        else:
+            return Response({"status": False, "message": "Invalid data provided."}, status=status.HTTP_400_BAD_REQUEST)
+
         
 
 
