8062f29561fa463866d4580c33e189bd87ddb6a5
Maryann Mwikali <mwikali119@gmail.com>
2023-09-20 11:21:15 +0300

    Correct org_id

--- organization/models.py
+++ organization/models.py
@@ -4,10 +4,7 @@ from django.core.validators import MinValueValidator
 # Create your models here.
 class OrganizationLunchWallet(models.Model):
   """Model for Organization Lunch Wallet"""
-  balance = models.DecimalField(max_digits=10,
-                                       decimal_places=2,
-                                       validators=[MinValueValidator(0)])
-  
-  organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+  balance = models.DecimalField(max_digits=10,decimal_places=2,validators=[MinValueValidator(0)])
+  org_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
--- withdrawals/models.py
+++ withdrawals/models.py
@@ -11,5 +11,6 @@ class Withdrawal(models.Model):
     amount = models.DecimalField(verbose_name='withdrawal amount',max_digits=10,decimal_places=2)
     created_at = models.DateTimeField(verbose_name='withdrawal timestamp', auto_now_add=True)
     updated_at=models.DateTimeField(verbose_name='updated timestamp',auto_now=True)
+    
     def __str__(self):
         return f"Withdrawal #{self.id}"
