c59da80c11908e1dbc1309252fcb17293067204f
JosephineMwende <josymwesh45@gmail.com>
2023-09-20 09:52:50 +0300

    updated the withdrawals model fields

--- withdrawals/models.py
+++ withdrawals/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 class Withdrawal(models.Model):
     status_choices = [
-        ('pending', "processing"),
-        ('completed', 'done')
+        ('redeemed', "Redeemed"),
+        ('not_redeemed', 'Not Redeemed'),
     ]
-    user_id = models.CharField(verbose_name='user ID', max_length=200)
-    status = models.CharField(verbose_name='withdrawal status', choices=status_choices, max_length=20)
-    amount = models.BigIntegerField(verbose_name='withdrawal amount')
+    user_id = models.IntegerField()
+    status = models.CharField(verbose_name='withdrawal status', choices=status_choices, max_length=20,default='not_redeemed')
+    amount = models.DecimalField(verbose_name='withdrawal amount',max_digits=10,decimal_places=2)
     created_at = models.DateTimeField(verbose_name='withdrawal timestamp', auto_now_add=True)
-
+    updated_at=models.DateTimeField(verbose_name='updated timestamp',auto_now=True)
     def __str__(self):
         return f"Withdrawal #{self.id}"
