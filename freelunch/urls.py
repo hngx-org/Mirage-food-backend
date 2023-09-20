a794d9824d76b512d7dff0f2354dd4c7c5877596
Collins Kasyoki <collinskasyoki@gmail.com>
2023-09-21 00:26:03 +0300

    Add swagger urls

--- freelunch/urls.py
+++ freelunch/urls.py
@@ -14,11 +14,48 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, re_path, include
+
 from django.contrib import admin
 from django.urls import path, include
+
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Schema view for DRF-YASG
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Music API",
+        default_version="v1",
+        description="A small API to display a bunch of music information",
+        # terms_of_service="https://www.yourapp.com/terms/",
+        # contact=openapi.Contact(email="contact@yourapp.com"),
+        # license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('organization.urls')),
     path('api/', include('users.urls')),
+
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json',
+    ),
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui',
+    ),
+    path(
+        'redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc',
+    ),
 ]
