c7b24904545df7a69c1a9832f23254d857e6c196
Uthman Ahmed <70240388+Hutty10@users.noreply.github.com>
2023-09-20 09:46:30 +0100

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,4 +1,3 @@
-import uuid
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -26,29 +25,32 @@ class UserManager(BaseUserManager):
 
 
 class User(PermissionsMixin, AbstractBaseUser):
-    id = models.UUIDField(_("user id"), editable=False, unique=True, primary_key=True, default=uuid.uuid4, db_index=True)
-    org_id = models.ForeignKey("organization.Organization", _("organization"), on_delete=models.CASCADE)
-    first_name = models.CharField(_("first name"), max_length=50)
-    last_name = models.CharField(_("last name"), max_length=50)
-    email = models.EmailField(_("email address"), max_length=254, db_index=True, unique=True)
-    phone = models.CharField(_("phone number"), max_length=255)
-    profile_pic = models.ImageField(_("profile picture"), upload_to=None)
-    password_hash = models.CharField(max_length=255)
-    is_admin = models.BooleanField(_("is admin"), default=False)
-    refresh_token = models.TextField(_("refresh token"))
-    bank_number = models.CharField(_("bank number"), max_length=255)
-    bank_code = models.CharField(_("bank code"), max_length=255)
-    bank_name = models.CharField(_("bank name"), max_length=255)
+    # by default django uses auto increament for the id
+    # uncomment org_id when organization model has been created
+    # org_id = models.ForeignKey("app.Model", verbose_name=_("organisation name"), on_delete=models.CASCADE, null=True)
+    first_name = models.CharField(_("first name"), max_length=225)
+    last_name = models.CharField(_("last name"), max_length=225, blank=True, null=True)
+    profile_pic = models.URLField(_("profile pic"),max_length=225, blank=True, null=True)
+    email = models.EmailField(_("email address"), max_length=225, db_index=True, unique=True)
+    phone = models.CharField(_("phone number"), max_length=20, null=True, blank=True)
+    password_hash = models.CharField(max_length=100)
+    is_admin = models.BooleanField(_("is admin"), blank=True, null=True)
+    refresh_token = models.TextField(_("refresh token"), blank=True, null=True)
+    bank_number = models.CharField(_("bank number"), max_length=50, blank=True, null=True)
+    bank_code = models.CharField(_("bank code"), max_length=50, blank=True, null=True)
+    bank_name = models.CharField(_("bank name"), max_length=50, blank=True, null=True)
     created_at = models.DateTimeField(_("created date"), auto_now_add=True)
     updated_at = models.DateTimeField(_("updated date"), auto_now=True)
+    is_deleted = models.BooleanField(_("deleted user"), default=False)
+    # launch_credit_balance = models.CharField(_("launch credit"), max_length=50)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name"]
     objects = UserManager()
 
     class Meta:
-        verbose_name = _("User")
-        verbose_name_plural = _("Users")
+        verbose_name = "User"
+        verbose_name_plural = "Users"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
