70a5ebec980a7074e71c3c05608d76ca818733a4
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-09-20 08:42:13 +0100

    abiola adeshina added media_url for profile picture

--- freelunch/settings.py
+++ freelunch/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = os.path.join(BASE_DIR, 'media/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
