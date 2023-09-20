ac48e5c1a31b91aa55a7023ae4140f8043f968f2
JosephJohncross <josephibochi@gmail.com>
2023-09-20 22:50:36 +0100

    Feature: Endpoint to get a lunch

--- freelunch/urls.py
+++ freelunch/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('organization.urls')),
     path('api/', include('users.urls')),
+    path('api/', include('lunches.urls')),
 ]
