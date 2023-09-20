a9d59b4d15fa926ad5cbdeb7684bd66d2fd8e4c6
johnpauljpc <jpcwork081@gmail.com>
2023-09-20 16:44:47 +0100

    endpoint for organization invite created

--- organization/urls.py
+++ organization/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("create", views.OrganizationView.as_view()),
+    path("invite/", views.OrganizationInviteView.as_view(), name="organization-invite"),
 ]
