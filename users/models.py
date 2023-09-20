1b9b7b03c07094da39f43ebe3657fc7b538c1c0c
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-09-20 03:31:01 +0100

    abiola adeshina did create user model part of it

--- users/models.py
+++ users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('Users must have an email address'))
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email, **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        if not extra_fields.get('is_staff'):
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if not extra_fields.get('is_superuser'):
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(models.Model):
+    pass
