798dc641ded8c2cfb36e2254983051a8f5bf743d
Maryann Mwikali <mwikali119@gmail.com>
2023-09-20 12:28:38 +0300

    Modify user table

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from cloudinary.models import CloudinaryField
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
@@ -15,11 +16,11 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password, **extra_fields):
-        # extra_fields.setdefault('org_id', 1)
-        # extra_fields.setdefault('is_admin',True)
+        extra_fields.setdefault('org_id', 1)
+        extra_fields.setdefault('is_admin',True)
         
-        # if not extra_fields.get('is_admin'):
-        #     raise ValueError(_('Superuser must have is_admin=True.'))
+        if not extra_fields.get('is_admin'):
+            raise ValueError(_('Superuser must have is_admin=True.'))
         
         return self.create_user(email, password, **extra_fields)
 
@@ -27,13 +28,12 @@ class UserManager(BaseUserManager):
 class User(PermissionsMixin, AbstractBaseUser):
     # by default django uses auto increament for the id
     # uncomment org_id when organization model has been created
-    # org_id = models.ForeignKey("app.Model", verbose_name=_("organisation name"), on_delete=models.CASCADE, null=True)
+    org_id = models.ForeignKey(Organization, verbose_name=_("organisation name"), on_delete=models.CASCADE, null=True)
     first_name = models.CharField(_("first name"), max_length=225)
     last_name = models.CharField(_("last name"), max_length=225, blank=True, null=True)
-    profile_pic = models.URLField(_("profile pic"),max_length=225, blank=True, null=True)
-    email = models.EmailField(_("email address"), max_length=225, db_index=True, unique=True)
+    profile_pic = CloudinaryField(_("profile pic"))
+    email = models.EmailField(_("email address"), max_length=225,unique=True)
     phone = models.CharField(_("phone number"), max_length=20, null=True, blank=True)
-    password_hash = models.CharField(max_length=100)
     is_admin = models.BooleanField(_("is admin"), blank=True, null=True)
     refresh_token = models.TextField(_("refresh token"), blank=True, null=True)
     bank_number = models.CharField(_("bank number"), max_length=50, blank=True, null=True)
@@ -41,8 +41,7 @@ class User(PermissionsMixin, AbstractBaseUser):
     bank_name = models.CharField(_("bank name"), max_length=50, blank=True, null=True)
     created_at = models.DateTimeField(_("created date"), auto_now_add=True)
     updated_at = models.DateTimeField(_("updated date"), auto_now=True)
-    is_deleted = models.BooleanField(_("deleted user"), default=False)
-    # launch_credit_balance = models.CharField(_("launch credit"), max_length=50)
+    lunch_credit_balance = models.CharField(_("lunch credit"), max_length=50)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name"]
