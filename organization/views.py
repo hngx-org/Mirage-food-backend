3c0717adcaecf3b8f739783c1d294187387b293f
Shulammite Odde <shullyd7@gmail.com>
2023-09-20 22:47:39 +0100

    Updated Endpoint

--- organization/views.py
+++ organization/views.py
@@ -2,6 +2,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Organization
 from users.models import User
+from .serializers import OrganizationSerializer
+
+
 # Create your views here.
 
 
@@ -15,14 +18,7 @@ def get_organization(request, user_id, organization_id):
     try:
         # Retrieve the organization based on user and organization IDs
         organization = Organization.objects.get(id=organization_id, user_id=user_id)
-        organization_data = {
-            'id': organization.id,
-            'name': organization.name,
-            'lunch_price': organization.lunch_price,
-            'currency': organization.currency,
-            'created_at': organization.created_at,
-            'updated_at': organization.updated_at
-        }
-        return Response(organization_data)
+        serializer = OrganizationSerializer(organization)  # Serialize the organization
+        return Response(serializer.data)
     except Organization.DoesNotExist:
         return Response({'error': 'Organization not found'}, status=status.HTTP_404_NOT_FOUND)
