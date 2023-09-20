aaf7c50bfba63851c31f5848c74532ca3fe7712d
Muftawu <mohammedyiwere@gmail.com>
2023-09-20 14:18:26 +0000

    update free lunch

--- lunches/views.py
+++ lunches/views.py
@@ -3,7 +3,8 @@ from rest_framework.decorators import api_view
 from .models import Lunch
 from rest_framework.response import Response
 from rest_framework import status
-from django.http import Http404
+from django.http import Http404 
+from .serializers import LunchSerializer
 
 # Create your views here.
 
@@ -15,3 +16,18 @@ def delete_free_lunch(id):
                 return Response({'Message': 'Free Lunch Deleted'}, status=status.HTTP_200_OK)
         except Lunch.DoesNotExist:
                 raise Http404("Free Lunch does not exist at the moment")
+
+
+@api_view(['PATCH'])
+def update_free_lunch(request, id):
+        try:
+                freelunch = Lunch.objects.get(id=id)
+        except Lunch.DoesNotExist:
+                return Response(f"No free lunch with id {id}")
+        
+        if request.method == "PATCH":
+                serializer = LunchSerializer(freelunch, data=request.data, partial=True)
+                if serializer.is_valid():
+                        serializer.save()
+                        return Response({"Lunch updated successfully": serializer.data}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
