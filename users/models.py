3a41acb5d9f5487877179cc872b35dc35154d489
Uthman Ahmed <70240388+Hutty10@users.noreply.github.com>
2023-09-20 07:17:59 +0100

    added user models

--- users/models.py
+++ users/models.py
@@ -1,31 +1,60 @@
+import uuid
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin,BaseUserManager
 from django.db import models
-from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 
 
-class CustomUserManager(BaseUserManager):
+class UserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError(_('Users must have an email address'))
+        if password is None:
+            raise ValueError(_("Password is compulsory"))
         email = self.normalize_email(email)
         user = self.model(
             email=email, **extra_fields
         )
         user.set_password(password)
         user.save(using=self.db)
+
         return user
 
     def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_active', True)
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('org_id', 'Mirage')
+        extra_fields.setdefault('isAdmin',True)
         
-        if not extra_fields.get('is_staff'):
-            raise ValueError(_('Superuser must have is_staff=True.'))
-        if not extra_fields.get('is_superuser'):
-            raise ValueError(_('Superuser must have is_superuser=True.'))
+        if not extra_fields.get('isAdmin'):
+            raise ValueError(_('Superuser must have isAdmin=True.'))
+        
         return self.create_user(email, password, **extra_fields)
 
 
-class CustomUser(models.Model):
-    pass
+class User(PermissionsMixin, AbstractBaseUser):
+    id = models.UUIDField(_("user id"),editable=False,unique=True,primary_key=True,default=uuid.uuid4,db_index=True,)
+    org_id = models.CharField(_("organisation name"), max_length=100,unique=True)
+    first_name = models.CharField(_("first name"), max_length=50)
+    last_name = models.CharField(_("last name"), max_length=50)
+    email = models.EmailField(_("email address"), max_length=254, db_index=True, unique=True)
+    phonenumber = models.PositiveIntegerField(_("phone number"))
+    profile_picture = models.ImageField(_("profile pi"), upload_to=None)
+    password_hash = models.CharField(max_length=100)
+    isAdmin = models.BooleanField(_("is admin"),default=False)
+    refresh_token = models.TextField(_("refresh token"))
+    launch_credit_balance = models.CharField(_("launch credit"), max_length=50)
+    bank_number = models.CharField(_("bank number"), max_length=50)
+    bank_code = models.CharField(_("bank code"), max_length=50)
+    bank_name = models.CharField(_("bank name"), max_length=50)
+    created_at = models.DateTimeField(_("created date"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("updated date"), auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
