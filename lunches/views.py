3e3c0c22739376231b770afaa8b0cdc9eb6f5f62
unknown <abdullahishuaibumaje.com>
2023-09-20 10:20:26 -0700

    Api endpoint for sending food and updating wallet balance

--- lunches/views.py
+++ lunches/views.py
@@ -1,15 +1,24 @@
 
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Lunch  # Import your Lunch model
-from .serializers import LunchSerializer  # Import your LunchSerializer
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.contrib.auth.models import User
+from .models import Lunch
+from .serializers import LunchSerializer
+import ast
+from organization.models import Organization, OrganizationLunchWallet
 
 class CreateFreeLunchAPIView(APIView):
+    authentication_classes = [JWTAuthentication]  # Use JWT Authentication
+    permission_classes = [IsAuthenticated]
+
     def post(self, request, *args, **kwargs):
         # Get data from the request body
-        sender = 1 # Assuming request.user is the sender
-        receivers = request.data.get('receivers', [])
+        user = request.user
+        receivers = request.data.get('receivers')
+        receivers_list = ast.literal_eval(receivers)
         quantity = request.data.get('quantity', 0)
         note = request.data.get('note', '')
 
@@ -17,38 +26,49 @@ class CreateFreeLunchAPIView(APIView):
         if not receivers:
             return Response({"message": "Receivers field is required"}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Prevent the user from sending lunch to themselves
-        if sender.id in receivers:
-            return Response({"message": "You cannot send lunch to yourself"}, status=status.HTTP_400_BAD_REQUEST)
-
         data = []
+        organization_id = 1  # Assuming organization ID is 1
 
-        for receiver_id in receivers:
+        try:
+            organization = Organization.objects.get(id=organization_id)
+            lunch_wallet = OrganizationLunchWallet.objects.get(org_id=organization)
+        except Organization.DoesNotExist:
+            return Response({"message": "Organization not found"}, status=status.HTTP_400_BAD_REQUEST)
+        except OrganizationLunchWallet.DoesNotExist:
+            return Response({"message": "Organization lunch wallet not found"}, status=status.HTTP_400_BAD_REQUEST)
+
+        for receiver_id in receivers_list:
             try:
-                lunch = Lunch.objects.create(sender=sender, receiver=receiver_id, note=note, quantity=quantity)
+                # Attempt to fetch the receiver instance based on the receiver_id
+                receiver = User.objects.get(id=int(receiver_id))
+
+                # Calculate the lunch cost
+                price = organization.lunch_price
+                total_cost = quantity * price
+
+                # Check if the organization has sufficient balance
+                if lunch_wallet.balance < total_cost:
+                    return Response({'detail': 'Insufficient balance in the organization lunch wallet.'}, status=status.HTTP_400_BAD_REQUEST)
+
+                # Create the Lunch object with the receiver instance
+                lunch = Lunch.objects.create(sender=user, receiver=receiver, note=note, quantity=quantity)
+
+                # Update the organization lunch wallet balance
+                lunch_wallet.balance -= total_cost
+                lunch_wallet.save()
+
+                # Serialize and append the lunch data
                 lunch_serialized = LunchSerializer(lunch)
                 data.append(lunch_serialized.data)
+            except User.DoesNotExist:
+                return Response({"message": f"User with ID {receiver_id} does not exist"}, status=status.HTTP_400_BAD_REQUEST)
             except Exception as e:
                 error = str(e)
-                return Response({'message': error}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                return Response({'message': error}, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response({"message": "Lunch request created successfully", "data": data}, status=status.HTTP_201_CREATED)
+        lunch_balance = lunch_wallet.balance
+        return Response({"message": "Lunch request created successfully",
+                         "statusCode": 201,
+                         "walletBalance": lunch_balance,
+                         "data": data})
 
-
-# Create your views here.
-class UpdateLunch(APIView):
-    def post(self, request):
-        # Get the authenticated user
-        user = request.user
-
-        # Get or create the lunch wallet for the user
-        lunch_wallet, created = Lunch.objects.get_or_create(user=user)
-
-        # Deserialize the data from the request
-        serializer = LunchSerializer(lunch_wallet, data=request.data)
-
-        if serializer.is_valid():
-            # Update the lunch wallet balance
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUES)
