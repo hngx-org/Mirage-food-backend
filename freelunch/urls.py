f645401be992536cf307b2fe1a81e4fbcf2803ad
theeduke <oscarian528@gmail.com>
2023-09-21 12:43:19 +0300

    endpoint for all the free lunches

--- freelunch/urls.py
+++ freelunch/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/organization/", include("organization.urls")),
+    path("api/", include("organization.urls")),
     path('api/', include('lunches.urls')),
 ]
