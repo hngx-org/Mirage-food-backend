9f0cb6c9435e82970f897bb8f67a6b394670aa53
abdulmalikadebayo <abdulmalikadebayo1@gmail.com>
2023-09-21 03:11:10 +0100

    registered the models

--- organization/admin.py
+++ organization/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Organization, OrganizationLunchWallet
 
 # Register your models here.
+admin.site.register(Organization)
+admin.site.register(OrganizationLunchWallet)
