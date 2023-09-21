1433546be03eec3ab1057ac15718f86705fb2de9
bensonisaac <oluchukwubensonisaac@gmail.com>
2023-09-21 06:14:50 +0100

    Update views.py

--- withdrawals/views.py
+++ withdrawals/views.py
@@ -2,48 +2,42 @@ from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-from lunches.models import Lunch
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Withdrawal
-from .serializers import WithdrawalSerializer
+from .serializers import WithdrawalSerializer, WithdrawalRequestSerializer
 
 
 class LunchWithdrawalCreateView(APIView):
+    permission_classes = [IsAuthenticated]
     def post(self, request, *args, **kwargs):
-        # I assummed the lunch_id is provided in the body
-        lunch_id = request.data.get("lunch_id")
-        try:
-            lunch = Lunch.objects.get(pk=lunch_id)
-        except Lunch.DoesNotExist:
-            return Response({"message": "Lunch not found"}, status.HTTP_404_NOT_FOUND)
+        serializer = WithdrawalRequestSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
 
-        if lunch.redeemed:
-            return Response(
-                {"message": "This lunch has already been redeemed"},
-                status=status.HTTP_400_BAD_REQUEST,
+            
+            withdrawal = Withdrawal.objects.create(
+                bank_name=serializer.validated_data["bank_name"],
+                bank_number=serializer.validated_data["bank_number"],
+                bank_code=serializer.validated_data["bank_code"],
+                amount=serializer.validated_data["amount"],
+                user_id=request.user
             )
-        # The code below withdraws the lunch. Payment provider wasn't integrated.
-
-        serializer = WithdrawalSerializer(data=request.data)
-        if serializer.is_valid():
-            withdrawal = serializer.save()
             withdrawal.status = "redeemed"
-            lunch.redeemed = True
             withdrawal.save()
+
 
             response_data = {
                 "id": withdrawal.id,
                 "user_id": withdrawal.user_id.id,
                 "status": withdrawal.status,
                 "amount": withdrawal.amount,
-                "created_at": withdrawal.created_at,
+                "created_at": withdrawal.created_at.isoformat(),
             }
 
             return Response(
                 {
                     "message": "Withdrawal request created successfully",
-                    "statusCode": 201,
+                    "statusCode": status.HTTP_201_CREATED,
                     "data": response_data,
                 },
                 status=status.HTTP_201_CREATED,
