562d76b9c5e0cb77931703503376b855daad051c
Muftawu <mohammedyiwere@gmail.com>
2023-09-21 14:55:48 +0000

    updated branch

--- lunches/urls.py
+++ lunches/urls.py
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 from django.urls import path 
+from .views import allFreeLunchesListView
 from . import views 
 
 urlpatterns = [
     path('update_lunch/<int:id>/', views.update_free_lunch, name='update_free_lunch'),
-=======
-from  django.urls import path
-from .views import allFreeLunchesListView
-
-urlpatterns = [
-   path("lunch/all", allFreeLunchesListView.as_view(), name="lunch-list"), 
->>>>>>> 5dec6a69ca57e6779e69eb9872c9c3600e6fc30f
+    path("lunch/all", allFreeLunchesListView.as_view(), name="lunch-list"), 
 ]
