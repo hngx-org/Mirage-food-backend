58d636bb7f9296501d9d3aaaa328d88937ee41d9
Precious <okolaaprecious@gmail.com>
2023-09-21 12:20:32 +0100

    Created signup serializer

--- users/serializers.py
+++ users/serializers.py
@@ -11,3 +11,14 @@ class CustomUserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = CustomUser.objects.create_user(**validated_data)
         return user
+    
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'first_name', 'last_name', 'phone_number')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
