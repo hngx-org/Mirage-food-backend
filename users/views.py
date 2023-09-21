5be8125ff4415767f69d130edfcae32ca38f5967
Precious <okolaaprecious@gmail.com>
2023-09-21 13:12:57 +0100

    updated views.py

--- users/views.py
+++ users/views.py
@@ -1,15 +1,14 @@
 # views.py
 
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .serializers import UserRegistrationSerializer
 
 class UserRegistrationView(APIView):
     def post(self, request):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
-            # You can optionally generate a JWT token here and return it in the response
-            return Response(status=status.HTTP_201_CREATED)
+            serializer.save()
+            return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
