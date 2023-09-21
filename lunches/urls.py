6dccf4367e8293528dfddcb35296b891d0178450
Maryann Mwikali <mwikali119@gmail.com>
2023-09-21 23:24:38 +0300

    Lunch withdrawals done

--- lunches/urls.py
+++ lunches/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, re_path, include
 from . import views
 
 urlpatterns = [
-    path('lunch/<int:id>', views.get_a_lunch, name='get_a_free_lunch'),
+    # path('lunch/<int:id>', views.get_a_lunch, name='get_a_free_lunch'),
 ]
