578c7b8bdffc31f776c7a965193910469feb5d85
Peter <obidaberechukwu@gmail.com>
2023-09-21 18:42:19 +0100

    merged

--- freelunch/urls.py
+++ freelunch/urls.py
@@ -19,5 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/organization/', include("organization.urls")),
+    path("api/", include("organization.urls")),
+    path('api/', include('lunches.urls')),
+    path('api/', include('users.urls'))
+
 ]
