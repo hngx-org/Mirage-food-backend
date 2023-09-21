85018dfe3d9713498c62631ed36e8dd8a2ff684c
Pundits-iTech <mohammedaalli088@gmail.com>
2023-09-21 15:26:16 +0000

    conflict resolve

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import UserRetrieveUpdateSet
 
 urlpatterns = [
-# path('users/', UserListViewSet.as_view(), name='user-list'),
+
 path('users/<int:pk>/', UserRetrieveUpdateSet.as_view(), name='user-retrieve-update-set'),
 ]
