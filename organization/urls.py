7eab69cac2abc5256a8dbfae4cac109d9c106966
Collins Kasyoki <collinskasyoki@gmail.com>
2023-09-21 19:55:30 +0300

    Update UserOrganizationAPI endpoint

--- organization/urls.py
+++ organization/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import OrganizationAPI
+from .views import OrganizationAPI, UserOrganizationAPI
 
 app_name = "organization"
 
@@ -10,8 +10,8 @@ router = DefaultRouter()
 router.register("", OrganizationAPI, basename="organization")
 
 urlpatterns = [
-    path("", include(router.urls)),
+    # path("", include(router.urls)),
     path("organization/<int:pk>/", OrganizationAPI.as_view({"put": "update"}), name="organization"),
-    path('users/<int:user_id>/organizations/<int:org_id>', views.get_organization, name='get-organization'),
+    path('users/<int:user_id>/organizations/<int:org_id>', UserOrganizationAPI.as_view(), name='get-organization'),
 
 ]
