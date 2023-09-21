51f9c25275fcd7a1541e50603d2ade12d9a7f637
Precious <okolaaprecious@gmail.com>
2023-09-21 13:12:24 +0100

    updated serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,23 @@
+# serializers.py
 
 from rest_framework import serializers
-from .models import User  
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('email', 'password', 'first_name', 'last_name', 'phone')
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+    def validate_password(self, value):
+        # Use Django's password validation to ensure a strong password
+        validate_password(value)
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
