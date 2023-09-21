e1d7debf1052f890a2d3df52fa9814b847503702
0layimika <110624406+0layimika@users.noreply.github.com>
2023-09-21 09:17:14 +0100

    get_specific_user_specific_lunch

--- lunches/views.py
+++ lunches/views.py
@@ -1,17 +1,11 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from .models import Lunch
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework import status
-from django.http import Http404
+from rest_framework.views import APIView
+from .models import Lunch
+from .serializer import LunchSerializer
 
-# Create your views here.
-
-@api_view(['DELETE'])
-def delete_free_lunch(id):
-        try:
-                freelunch = Lunch.objects.get(id=id)
-                freelunch.delete()
-                return Response({'Message': 'Free Lunch Deleted'}, status=status.HTTP_200_OK)
-        except Lunch.DoesNotExist:
-                raise Http404("Free Lunch does not exist at the moment")
+class LunchDetailView(APIView):
+    def get(self, request, user_id, lunch_id):
+        lunch = get_object_or_404(Lunch, sender_id=user_id, id=lunch_id)
+        serializer = LunchSerializer(lunch)
+        return Response(serializer.data)
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DeleteUserView
-
+# from .views import DeleteUserView
+from lunches.views import LunchDetailView
 urlpatterns = [
-    path('users/<int:id>/', DeleteUserView.as_view()),
+    # path('users/<int:id>/', DeleteUserView.as_view()),
+    path('users/<int:user_id>/lunches/<int:lunch_id>',LunchDetailView.as_view(),name='lunch-detail'),
 ]
