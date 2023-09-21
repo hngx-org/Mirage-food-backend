d9ba44dee16268d824cfd6203fff778dc79c2958
Burger-karl <charleskalu863@gmail.com>
2023-09-21 11:45:22 +0100

    ft-delete_users

--- users/views.py
+++ users/views.py
@@ -12,13 +12,13 @@ class DeleteUserView(APIView):
             return User.objects.get(pk=id)
         except:
             return Response({
-                'error': 'User does not exist'
+                'error': 'User does not exist.'
             }, status=status.HTTP_404_NOT_FOUND)
 
 
     def delete_user(self, request, pk):
         user = self.get_user_by_pk(pk=id)
         user.delete()
-        return Response({'Message': 'User is Deleted'}, status=status.HTTP_204_NO_CONTENT)
+        return Response({'Message': 'User Deleted'}, status=status.HTTP_204_NO_CONTENT)
 
 
