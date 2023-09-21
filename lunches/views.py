23d21cbf3a84afa03502ea5e76fe54d8d71d8358
unknown <abdullahishuaibumaje.com>
2023-09-21 09:23:41 -0700

    Ignored updating lunch wallet balance

--- lunches/views.py
+++ lunches/views.py
@@ -14,12 +14,12 @@ from rest_framework.permissions import AllowAny
 
 class CreateFreeLunchAPIView(APIView):
     #
-    # permission_classes = [AllowAny]
+    permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
         # Get data from the rest body
-        #user = request.user
-        user = User.objects.get(email="abdullahishuaibumaje@gmail.com")
+        user = request.user
+        #user = User.objects.get(email="abdullahishuaibumaje@gmail.com")
         user_id=user.id
         receivers = request.data.get('receivers')
         try:
@@ -37,21 +37,25 @@ class CreateFreeLunchAPIView(APIView):
         data = []
         
         for receiver_id in receivers_list:
+            # skip if the user is trying to send to himself
+            if receiver_id==user_id:
+                continue
+            else:
     
-            try:
-                # Attempt to fetch the receiver instance based on the receiver_id
-                receiver = User.objects.get(id=int(receiver_id))
+                try:
+                    # Attempt to fetch the receiver instance based on the receiver_id
+                    receiver = User.objects.get(id=int(receiver_id))
 
-                # Create the Lunch object with the receiver instance
-                lunch = Lunch.objects.create(sender_id=user, receiver=receiver, note=note, quantity=quantity)
-                # Serialize and append the lunch data
-                lunch_serialized = LunchSerializer(lunch)
-                data.append(lunch_serialized.data)
-            except User.DoesNotExist:
-                return Response({"message": f"User with ID {receiver_id} does not exist"}, status=status.HTTP_400_BAD_REQUEST)
-            except Exception as e:
-                error = str(e)
-                return Response({'message': error}, status=status.HTTP_400_BAD_REQUEST)
+                    # Create the Lunch object with the receiver instance
+                    lunch = Lunch.objects.create(sender_id=user, receiver=receiver, note=note, quantity=quantity)
+                    # Serialize and append the lunch data
+                    lunch_serialized = LunchSerializer(lunch)
+                    data.append(lunch_serialized.data)
+                except User.DoesNotExist:
+                    return Response({"message": f"User with ID {receiver_id} does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+                except Exception as e:
+                    error = str(e)
+                    return Response({'message': error}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response({"message": "Lunch request created successfully",
                          "statusCode": 201,
