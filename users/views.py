47c242304b12ae9b63437e63c0d5ff6c4ab9a943
Chephos <efosacharlesabu@gmail.com>
2023-09-22 08:44:37 +0100

    feat: get user details implemented but not tested

--- users/views.py
+++ users/views.py
@@ -1,6 +1,5 @@
 from rest_framework.response import Response
-from .serializers import UserRegistrationSerializer,UserListSerializer
-from .models import User
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.authtoken.models import Token
@@ -9,15 +8,22 @@ from rest_framework.permissions import AllowAny
 from django.contrib.auth import authenticate, login
 
 
-from .serializers import SearchedUserSerializer
-from django.http import Http404
+from .models import User
+from .serializers import (
+    UserRegistrationSerializer,
+    UserListSerializer,
+    SearchedUserSerializer,
+    UserDetailsSerializer,
+)
+from . import workers
+
 
 # Create your views here.
 class LoginView(APIView):
     def post(self, request, *args, **kwargs):
-    # Get username and password from the request
-        email = request.data.get('email')
-        password = request.data.get('password')
+        # Get username and password from the request
+        email = request.data.get("email")
+        password = request.data.get("password")
 
         # Authenticate the user
         user = authenticate(request, email=email, password=password)
@@ -26,31 +32,34 @@ class LoginView(APIView):
             # If authentication is successful, create or retrieve a token
             token, created = Token.objects.get_or_create(user=user)
             login(request, user)  # Optional: Log the user in
-            return Response({'token': token.key}, status=status.HTTP_200_OK)
+            return Response({"token": token.key}, status=status.HTTP_200_OK)
         else:
-            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response(
+                {"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED
+            )
 
 
 class DeleteUserView(APIView):
-
     def get_user_by_pk(self, pk):
         try:
             return User.objects.get(pk=id)
         except:
-            return Response({
-                'error': 'User does not exist.'
-            }, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"error": "User does not exist."}, status=status.HTTP_404_NOT_FOUND
+            )
 
     def delete_user(self, request, pk):
         user = self.get_user_by_pk(pk=id)
         user.delete()
-        return Response({'Message': 'User Deleted'}, status=status.HTTP_204_NO_CONTENT)
+        return Response({"Message": "User Deleted"}, status=status.HTTP_204_NO_CONTENT)
 
 
 class UserRegistrationView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserRegistrationSerializer
     permission_classes = [AllowAny]
+
+
 class UserListViewSet(APIView):
     def get(self, request, *args, **kwargs):
         """
@@ -59,17 +68,20 @@ class UserListViewSet(APIView):
         queryset = User.objects.all()
         serializer = UserListSerializer(queryset, many=True)
 
-        return Response({
-            "message": "successfully fetched users",
-            "statusCode": status.HTTP_200_OK,
-            "data": serializer.data
-        }, status=status.HTTP_200_OK)
+        return Response(
+            {
+                "message": "successfully fetched users",
+                "statusCode": status.HTTP_200_OK,
+                "data": serializer.data,
+            },
+            status=status.HTTP_200_OK,
+        )
 
 
 class SearchUserView(APIView):
     "Api view accepting either a name (first or last) or email parameter to search for a user"
 
-    def get_object(self, param:str):
+    def get_object(self, param: str):
         try:
             return User.objects.get(first_name=param)
         except User.DoesNotExist:
@@ -81,12 +93,33 @@ class SearchUserView(APIView):
                 except User.DoesNotExist:
                     raise Http404
 
-    def get(self, request, name_or_email:str, *args, **kwargs):
+    def get(self, request, name_or_email: str, *args, **kwargs):
         instance = self.get_object(name_or_email)
         serializer = SearchedUserSerializer(instance)
-        return Response({
-            'message': 'User Found',
-            'statusCode': status.HTTP_200_OK,
-            'data': serializer.data
-        }, status=status.HTTP_200_OK)
+        return Response(
+            {
+                "message": "User Found",
+                "statusCode": status.HTTP_200_OK,
+                "data": serializer.data,
+            },
+            status=status.HTTP_200_OK,
+        )
 
+
+class UserDetailView(APIView):
+    def get(self, request):
+        """
+        Get user details
+        """
+        user = request.user
+        user_details = workers.UserWorker.get_user_details(user.id)
+        serializer = UserDetailsSerializer(user_details)
+        return Response(
+            {
+                "message": "User data fetched",
+                "statusCode": status.HTTP_200_OK,
+                "data": serializer.data,
+            },
+            status=status.HTTP_200_OK,
+            message="User data fetched",
+        )
