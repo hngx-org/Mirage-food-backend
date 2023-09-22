a878dc312379ed98306751fa2e051ea3d4a683a2
Tega Rorobi <rorobitega@gmail.com>
2023-09-22 20:54:00 +0100

    Made minor changes to the withdrawals app, endpoint at /api/withdrawal/request/ working fine.

--- withdrawals/urls.py
+++ withdrawals/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import WithdrawalUpdateView, LunchWithdrawalCreateView
 
 urlpatterns = [
-    path('withdrawal/request', LunchWithdrawalCreateView.as_view(), name='withdrawal-create'),
+    path('withdrawal/request/', WithdrawalCreateView.as_view(), name='withdrawal-create'),
     path('withdrawals/<int:pk>/', WithdrawalUpdateView.as_view(), name='withdrawal-update'),
 ]
