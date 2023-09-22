895f32a4e3d03122f9f6d2ffe2a830cbf77cf2ab
Maryann Mwikali <mwikali119@gmail.com>
2023-09-22 15:28:09 +0300

    Change DB settings

--- freelunch/settings.py
+++ freelunch/settings.py
@@ -91,7 +91,8 @@ DATABASES = {
         "NAME": config("DB_NAME"),
         "USER": config("DB_USER"),
         "PASSWORD": config("DB_PASSWORD"),
-        "port":"3306"
+        "port":"3306",
+        "HOST": config("DB_HOST")
     }
 }
 
