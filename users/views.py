78164fd5a4593b3fb6d6c64ef160f66cc1776a39
Pundits-iTech <mohammedaalli088@gmail.com>
2023-09-22 14:36:16 +0000

    final commit for the user-update(PUT & PATCH) endpoint

--- users/views.py
+++ users/views.py
@@ -8,6 +8,7 @@ from .serializers import UserListSerializer
 
 # Beginning of the user update
 class UserRetrieveUpdateSet(APIView):
+   
     def get_object(self, id):
         """
         Gets a user object with the given pk
@@ -24,7 +25,7 @@ class UserRetrieveUpdateSet(APIView):
         user = self.get_object(id)
         if user is None:
             return Response({
-                "message": f"User with id {id} does not exist.",
+                "message": f"User with pk {id} does not exist.",
                 "statusCode": status.HTTP_404_NOT_FOUND,
             }, status=status.HTTP_404_NOT_FOUND)
 
@@ -36,12 +37,36 @@ class UserRetrieveUpdateSet(APIView):
             "data": serializer.data
         }, status=status.HTTP_200_OK)
 
-class DeleteUserView:
+    def put(self, request, id, format=None):
+        """
+        Update the user details
+        """
+        user = self.get_object(id)
+        serializer = UserListSerializer(user, data=request.data)
 
-    def get_user_by_pk(self, id):
-        try:
-            return User.objects.get(id=id)
-        except:
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "message": "Successfully updated user information",
+                "statusCode": status.HTTP_200_OK,
+                "data": serializer.data
+            }, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# the patch function
+    '''
+    The patch method is responsible for performing the patch operation on the endpoint.
+    That it make it possible to partially update the user details
+    '''
+
+    def patch(self, request, id, format=None):
+        """
+        Partially update user details
+        """
+        user = self.get_object(id)
+
+        if user is None:
             return Response({
                 "message": f"User with pk {id} does not exist.",
                 "statusCode": status.HTTP_404_NOT_FOUND,
