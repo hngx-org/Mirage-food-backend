47e545359233646d01c2b4a39b0e87893e947b5c
Maryann Mwikali <mwikali119@gmail.com>
2023-09-22 09:47:09 +0300

    Search user works

--- users/urls.py
+++ users/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('users/all', UserListViewSet.as_view(), name='users-list'),
     path('users/<int:id>/', DeleteUserView.as_view()),
     path('users/<int:user_id>/lunches/<int:lunch_id>',LunchDetailView.as_view(),name='lunch-detail'),
-    path('search/<str:name_or_email>/', SearchUserView.as_view(), name='search-users'),
+    path('user/search/<str:name_or_email>/', SearchUserView.as_view(), name='search-users'),
     path('auth/user/signup/', UserRegistrationView.as_view(), name='user-signup'),
     path('auth/login/', LoginView.as_view(), name='login')
 
