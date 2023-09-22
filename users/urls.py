1fe22527d3d9b69afe2055e5d97fa12b827f2777
Maryann Mwikali <mwikali119@gmail.com>
2023-09-22 09:42:04 +0300

    Fetch users works

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,7 @@ from .views import UserRegistrationView
 
 
 urlpatterns = [
-    path('users/', UserListViewSet.as_view(), name='users-list'),
+    path('users/all', UserListViewSet.as_view(), name='users-list'),
     path('users/<int:id>/', DeleteUserView.as_view()),
     path('users/<int:user_id>/lunches/<int:lunch_id>',LunchDetailView.as_view(),name='lunch-detail'),
     path('search/<str:name_or_email>/', SearchUserView.as_view(), name='search-users'),
