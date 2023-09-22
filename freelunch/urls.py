fc1cc6e17537ebd968e873a3168e5678ab2d5fa1
Pundits-iTech <mohammedaalli088@gmail.com>
2023-09-22 01:12:11 +0000

    final commit for the user-update(PUT & PATCH) endpoint

--- freelunch/urls.py
+++ freelunch/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("organization.urls")),
     path('api/', include('lunches.urls')),
-    path('api/',include('users.urls'))
+    path('api/',include('users.urls')),
 
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,6 @@
 from django.urls import path
-<<<<<<< HEAD
+
 from .views import UserRetrieveUpdateSet
-
-urlpatterns = [
-
-path('users/<int:id>/', UserRetrieveUpdateSet.as_view(), name='user-retrieve-update-set'),
-]
-=======
 from .views import DeleteUserView
 from django.urls import path
 from .views import UserListViewSet
@@ -14,7 +8,8 @@ from lunches.views import LunchDetailView
 
 urlpatterns = [
     path('users/', UserListViewSet.as_view(), name='users-list'),
+    path('users/<int:id>/', UserRetrieveUpdateSet.as_view(), name='user-retrieve-update-set'),
     path('users/<int:id>/', DeleteUserView.as_view()),
     path('users/<int:user_id>/lunches/<int:lunch_id>',LunchDetailView.as_view(),name='lunch-detail'),
 ]
->>>>>>> 5dec6a69ca57e6779e69eb9872c9c3600e6fc30f
+
