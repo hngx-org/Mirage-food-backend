913be02accf16d343c6469b0d92a17dfbef4da9b
kurves <kavssimply@gmail.com>
2023-09-23 08:21:38 +0300

    env settings

--- freelunch/settings.py
+++ freelunch/settings.py
@@ -30,7 +30,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY",default="mydefaultvalue")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
