3c74509f1cfedab360d26588afbee29efa9130ee
unknown <abdullahishuaibumaje.com>
2023-09-25 16:03:59 -0700

    Tweaked the urls

--- withdrawals/urls.py
+++ withdrawals/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import WithdrawalUpdateView, WithdrawalCreateView
 
 urlpatterns = [
-    path('withdrawal/request', WithdrawalCreateView.as_view(), name='withdrawal-create'),
-    path('withdrawals/<int:pk>', WithdrawalUpdateView.as_view(), name='withdrawal-update'),
+    path('withdrawal/request/', WithdrawalCreateView.as_view(), name='withdrawal-create'),
+    path('withdrawals/<int:pk>/', WithdrawalUpdateView.as_view(), name='withdrawal-update'),
 ]
